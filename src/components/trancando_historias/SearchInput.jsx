4fa020d3860b390ef8cbbc0bc058d1bdf8620e1e
davifeitosa0 <davi.rodrigues@sptech.school>
2024-09-15 23:09:35 -0300

    fix: ajustando o input de search para a lupa não sair dele

--- src/components/trancando_historias/SearchInput.jsx
+++ src/components/trancando_historias/SearchInput.jsx
@@ -5,7 +5,7 @@ import React, { forwardRef } from "react";
 
 const SearchInput = forwardRef(({onKeyDown,className ,...props}, ref) => {
   return (
-    <div className="relative">
+    <div className="relative w-fit">
       <Input ref={ref} className={clsx("pr-8", className)} onKeyDown={onKeyDown} {...props} />
       <Search className="absolute top-0 right-2"height={"100%"} />
     </div>
