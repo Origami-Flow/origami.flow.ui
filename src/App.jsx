75e2247bb279b5a256722b9fb5afdd5d2affaa43
luangomesx <luan.araujo@sptech.school>
2024-09-30 19:32:08 -0300

    Criação da tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./routes/Home/page";
+import Login from "./routes/Home/Login";
 
 function App() {
   return (
     <BrowserRouter> 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>  
   );
