9d776c94da6f1849483df32cef533ed39e2be17e
luangomesx <luan.araujo@sptech.school>
2024-09-30 19:32:08 -0300

    Criação da tela de login

---
+++ src/routes/Home/Login.jsx
@@ -0,0 +1,12 @@
+import HeaderText from "../../components/HeaderText";
+import Header from "../../components/Header";
+
+const Login = () => {
+  return (
+      <div>
+        <Header />
+        <HeaderText text="Login" />
+      </div>
+  );
+};
+export default Login;
