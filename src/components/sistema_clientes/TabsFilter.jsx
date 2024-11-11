1f0070bfc634e891b7543328970aca5e2a8f4555
davifeitosa0 <davi.rodrigues@sptech.school>
2024-11-11 19:50:04 -0300

    feat - adicionando calendario de semandas

--- src/components/sistema_clientes/TabsFilter.jsx
+++ src/components/sistema_clientes/TabsFilter.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const TabsFilter = ({ tabs, initialTab }) => {
+const TabsFilter = ({ tabs, initialTab, setMedidaTemporal }) => {
     const [activeTab, setActiveTab] = useState(initialTab || 0);
 
     const handleTab = (tab) => {
         setActiveTab(tab);
     };
 
+    useEffect(() => {
+        setMedidaTemporal(tabs[activeTab]?.text);
+    }, [activeTab]);
+
     return (
         <ul className="w-[40%] h-[8vh] flex items-center justify-between border-gray-300 border-[0.5px] border-solid rounded-xl p-2 mb-4 max-md:w-full max-md:h-[7vh]">
-            {tabs.map((tab, index) => (
+            {tabs?.map((tab, index) => (
                 <li key={index} className={`w-[50%] p-2 max-lg:p-1 list-none text-center cursor-pointer transition-colors duration-300 ease-in-out rounded-xl max-md:text-sm ${activeTab === index ? 'bg-roseprimary text-branconeutro' : 'text-black/65'}`} onClick={() => handleTab(index)}>
                     {tab.text}
                 </li>
--- src/routes/Agendamentos/page.jsx
+++ src/routes/Agendamentos/page.jsx
@@ -4,14 +4,35 @@ import Header from "@/components/shared/header_sistema/Header";
 import TabsFilter from "@/components/sistema_clientes/TabsFilter";
 import { horas, MESES } from "@/utils/datas";
 import { ChevronLeft, ChevronRight, Plus } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { format, addDays, startOfWeek } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
 const Agendamentos = () => {
-  const [medidaTemporal, setMedidaTemporal] = useState("Semana");
+  const [medidaTemporal, setMedidaTemporal] = useState(1);
   const [mesAtual, setMesAtual] = useState(new Date().getMonth());
   const [anoAtual, setAnoAtual] = useState(new Date().getFullYear());
+  const tabs = [{ text: "Semana" }, { text: "Mês" }, { text: "Ano" }];
+  const containerRef = useRef(null);
+
+  const events = [
+    {
+      title: "Reunião",
+      start: new Date("2024-11-13T08:30:00"),
+      end: new Date("2024-11-13T12:00:00"),
+    },
+  ];
+
+  useEffect(() => {
+    console.log(medidaTemporal);
+  }, [medidaTemporal]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = 350;
+    }
+  }, []);
+
 
   const handleMesAnterior = () => {
     if (mesAtual === 0) {
@@ -31,12 +52,6 @@ const Agendamentos = () => {
     }
   };
 
-  const tabs = [
-    { text: "Semana" },
-    { text: "Mês" },
-    { text: "Ano" },
-  ];
-
   const legendItems = [
     { colorClass: "bg-blue-500", label: "Pessoal" },
     { colorClass: "bg-purple-700", label: "Trabalho" },
@@ -53,6 +68,29 @@ const Agendamentos = () => {
     };
   });
 
+
+  const clearDays = days.map((day) => {
+    return day.label.replace(/[^0-9]+/g, '');
+  })
+  const convertHoursInNumber = (horas) => {
+    let horasLimpas = horas.replace('h', '');
+    return parseInt(horasLimpas);
+  }
+
+  const calculateEventPositionAndHeight = (event, cellHeight) => {
+    const startHour = event.start.getHours() + event.start.getMinutes() / 60;
+    const endHour = event.end.getHours() + event.end.getMinutes() / 60;
+
+    const startOffset = (startHour % 1) * cellHeight;
+    const durationInHours = endHour - startHour;
+    const height = durationInHours * cellHeight;
+
+    return {
+      top: startOffset,
+      height: height,
+    };
+  };
+
   return (
     <main className="flex flex-col items-center justify-start relative pl-44 h-screen max-md:pl-0 max-md:pb-24 ">
       <Header />
@@ -60,7 +98,7 @@ const Agendamentos = () => {
         <div className="flex w-52 items-center gap-2">
           <div className="h-fit flex flex-row gap-2">
             <h1 className="font-laisha h-fit text-2xl">Próximos agendamentos</h1>
-            <div className="rounded-full flex self-center size-8 p-1 bg-roseprimary">
+            <div className="rounded-full flex self-center size-8 p-1 bg-roseprimary cursor-pointer hover:scale-105 ease-linear">
               <Plus color="white" />
             </div>
           </div>
@@ -77,23 +115,23 @@ const Agendamentos = () => {
               </Button>
             </div>
             <div className="w-[80%] flex justify-center">
-              <TabsFilter tabs={tabs} initialTab={1} />
+              <TabsFilter tabs={tabs} initialTab={0} setMedidaTemporal={setMedidaTemporal} />
             </div>
           </div>
         </div>
 
         <div>
           {legendItems.map((item, index) => (
-            <TipoAgendamento className={item.colorClass} tipo={item.label} />
+            <TipoAgendamento key={index} className={item.colorClass} tipo={item.label} />
           ))}
         </div>
       </div>
 
 
 
-      <div className=" w-[90%] p-4 mt-8 h-full max-h-full">
-        <div className="grid grid-cols-8 text-center">
-          <div className="col-span-1"></div> 
+      <div className=" w-[90%] mt-8 max-h-[73%] overflow-auto" ref={containerRef}>
+        <div className="grid grid-cols-8 text-center sticky top-0 bg-white z-20 border-b">
+          <div className="col-span-1"></div>
           {days.map((day, index) => (
             <div
               key={index}
@@ -107,13 +145,27 @@ const Agendamentos = () => {
         <div className="grid grid-cols-8">
           {horas.map((hour) => (
             <React.Fragment key={hour}>
-              <div className="flex items-center border justify-end">{hour}</div>
-              {days.map((_, dayIndex) => (
+              <div className="flex border justify-end">{hour}</div>
+              {clearDays.map((day, dayIndex) => (
                 <div
                   key={`${hour}-${dayIndex}`}
-                  className="border-t border-r h-12"
+                  className="border-t border-r h-12 relative"
                 >
-
+                  {events.map((event, eventIndex) => {
+                    if (event.start.getDate() == day && event.start.getHours() === convertHoursInNumber(hour)) {
+                      const { top, height } = calculateEventPositionAndHeight(event, 48);
+                      return (
+                        <div
+                          key={eventIndex}
+                          className="border-b bg-purple-400 text-center absolute w-full z-10"
+                          style={{ top: `${top}px`, height: `${height}px` }}
+                        >
+                          {event.title}
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
                 </div>
               ))}
             </React.Fragment>
